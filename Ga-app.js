fd0672dff89f5b5eb35ccb9bf81fb0c82c25aa61
Olivia Anderson <liv.and1012@gmail.com>
2023-05-01 10:01:55 -0400

    adding changes

---
+++ Ga-app.js
@@ -0,0 +1,12 @@
+const express =require('express')
+
+const hostname= 'localhost'
+const Port= 3000
+const app= express()
+
+// const sequelize= new Hosptial('Patients','Doctors', 'Appointment')
+
+app.listen(
+    Port,
+   
+)
